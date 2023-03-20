5c41a07f0117e6d039b4b0b8a346d5cc0438e8bd
301inspur <z1652091948@outlook.com>
2023-03-20 20:49:40 +0800

    add logger init

--- core/run.go
+++ core/run.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/pipikai/yun/common/config"
+	"github.com/pipikai/yun/common/logger"
 	"github.com/pipikai/yun/core/storage"
 	"github.com/pipikai/yun/core/tracker"
 	"github.com/spf13/viper"
@@ -11,6 +12,10 @@ import (
 
 func Start() {
 	config.InitViper()
+
+	logger.InitLogger()
+	defer logger.Logger.Sync()
+
 	switch viper.GetString("ServiceType") {
 	case "storage":
 		storage.Run()
