f66dca140f6654f875dd4f59f50e2cb7e69c767b
301inspur <z1652091948@outlook.com>
2023-03-20 20:49:23 +0800

    add result format setting

---
+++ common/util/result.go
@@ -0,0 +1,24 @@
+package util
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/pipikai/yun/common/logger"
+)
+
+type ResponseStruct struct{}
+
+var Response = ResponseStruct{}
+
+func (ResponseStruct) ResponsFmt(ctx *gin.Context, httpStatus int, code int, data gin.H, msg string) {
+	ctx.JSON(httpStatus, gin.H{"code": code, "msg": msg, "data": data})
+}
+func (r ResponseStruct) Success(ctx *gin.Context, data gin.H, msg string) {
+	r.ResponsFmt(ctx, http.StatusOK, 200, data, msg)
+}
+func (r ResponseStruct) Error(ctx *gin.Context, data gin.H, msg string) {
+	logger.Logger.Error(msg)
+
+	r.ResponsFmt(ctx, http.StatusBadRequest, 400, data, msg)
+}
