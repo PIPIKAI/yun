ad643a4b9d14868257422e09df69abcdda6b289d
pipikai <z1652091948@outlook.com>
2023-04-18 16:25:33 +0800

    add db tets

---
+++ yun_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"testing"
+	"time"
+
+	"github.com/pipikai/yun/common/leveldb"
+	"github.com/pipikai/yun/core/tracker/models"
+)
+
+func TestUpdataOne(t *testing.T) {
+	err := leveldb.UpdataOne(models.FileInfo{
+		ID:      "testid",
+		Storage: "testStorage",
+		FileMeta: models.FileMeta{
+			Md5:         "testMd5",
+			Size:        123456,
+			CreatedTime: time.Now(),
+		},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestGetOne(t *testing.T) {
+	v, err := leveldb.GetOne[models.FileInfo]("testid")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(v)
+}
+
+func TestGetAll(t *testing.T) {
+	v, err := leveldb.GetAll[models.FileInfo]()
+	if err != nil {
+		t.Fatal(err)
+	}
+	res, _ := json.Marshal(v)
+	t.Log(string(res))
+}
+
+func TestDeleteOne(t *testing.T) {
+	err := leveldb.DeleteOne[models.FileInfo]("testid")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
